061def2cd817ef8dcb71425294197cde6a9cf7ef
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-02-02 21:20:48 +0100

    weather icon- sreach city

--- src/app.js
+++ src/app.js
@@ -79,3 +79,12 @@ function formatDate(timestamp) {
     `;
     }
   }
+
+  function search(city) {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+  
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(dispalyForecast);
+  }
