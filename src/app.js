51882a0faec8c6e5479bd1e1148a8513d3c2bba8
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-02-05 20:21:14 +0100

    app.js done

--- src/app.js
+++ src/app.js
@@ -1,57 +1,4 @@
-function formatDate (timestamp){
-
-  let date = new date (timestamp);
-  let hours = date.getHours();
-    if(hours < 10){
-      hours = '0${hours}';
-    }
-
-  let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = '0${minutes}';
-    }
-
-  let days = ["Sunday",
-    "Monday", 
-    "Tuesday", 
-    "Wensday", 
-    "Thursday", 
-    "Friday",
-    "Saturday"];
-  let day = days[date.getDay()];
-
-return '${day} ${hours}:${minutes} ' ;
-}
-
-
-
-
-
-function displayTemperature(response){
-console.log(response.data);
-let temperatureElement =  document.querySelector("#temperature");
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let windElement = document.querySelector("#wind");
-let dateElement = document.querySelector("#date");
-
-temperatureElement.innerHTML=Math.round(response.data.main.temp);
-cityElement.innerHTML = response.data.name;
-descriptionElement.innerHTML = response.data.weather[0].description;
-humidityElement.innerHTML = response.data.main.humidity;
-windElement.innerHTML = Math.round(response.data.wind.speed);
-dateElement.innerHTML = formatDate(response.date.dt * 1000);
-}
-
-let apiKey = "9f2c64f1ec67e9d10d3043d48f9aa497";
-let apiUrl = 'api.openweathermap.org/data/2.5/weather?q=${city},uk&appid={API key}&units=metric';
-
-
-axios.get(apiUrl).then(displayTemperature);
-
-
-/*function formatDate(timestamp) {
+function formatDate(timestamp) {
     let date = new Date(timestamp);
   
     let days = [
@@ -181,4 +128,4 @@ axios.get(apiUrl).then(displayTemperature);
   let celsiusLink = document.querySelector("#celsius-link");
   celsiusLink.addEventListener("click", displayCelsiusTemperature);
   
-  search("New York");*/
+  search("New York");
